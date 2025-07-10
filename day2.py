88e54c854d751cb998f2030bab18e7d4297662a7
krashmi006 <k.rashmii006@gmail.com>
2025-07-10 13:53:58 +0545

    day 2 project

---
+++ day2.py
@@ -0,0 +1,21 @@
+name = "Rashmi"
+print(name[:3])
+
+print(len(name))
+
+#find and replace
+print(name.find("h"))
+replaced = name.replace("h","m")
+print(replaced)
+
+#escape sequence character
+text = "My name is Rashmi \n and i'm 19 years old"
+print(text)
+
+#1. wap to detect double spaces in a string 
+#2. replace the double spaces from problem 3 with single spaces
+#3. wap to format the following letter using escape sequence characters. 
+#letter = "Dear students, this python course is going well. Thanks!"
+
+
+
